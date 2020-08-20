0d175629bb9ee81bbc18aa521875d48da51cb738
wangyaowu1994 <584030001@qq.com>
2020-08-20 09:35:10 +0800

    20200820 过滤器 by王耀武

---
+++ marketadmin/src/main/java/com/cykj/marketadmin/interceptor/LoginInterceptor.java
@@ -0,0 +1,23 @@
+package com.cykj.marketadmin.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Component
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("admin") == null) {
+            String path = request.getContextPath();
+            request.getRequestDispatcher("../jsp/login.jsp").forward(request, response);
+            return false;
+        }
+        return true;
+    }
+}
---
+++ marketadmin/src/main/java/com/cykj/marketadmin/interceptor/LoginInterceptorConfig.java
@@ -0,0 +1,17 @@
+package com.cykj.marketadmin.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class LoginInterceptorConfig  extends WebMvcConfigurerAdapter {
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/login","/js/**","/static/**","/loginControl/login");
+    }
+}
